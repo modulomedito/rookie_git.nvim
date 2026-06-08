46fea1f3bea2560b2055a4bafab9701f69f01a72
modulomedito <chcchc1995@outlook.com>
2026-06-08 14:53:42 +0800

    🦄 refactor(abbr):[#]run command abbreviations asynchronously via RkGitAsync
    - Cabbrevs previously used fugitive `G` commands which blocked Neovim until the git operation
      completed.
    - Added `RkGitAsync` user command backed by `async_git` which runs git via `jobstart` and notifies
      on completion, keeping the UI responsive.
    - All 14 cabbrevs now expand to `RkGitAsync ...`; `silent` is no longer needed since async output
      does not echo in the message area.

--- lua/rookie_git/abbr.lua
+++ lua/rookie_git/abbr.lua
@@ -1,5 +1,25 @@
 local M = {}
 
+local function async_git(args, success_msg)
+    local cmd_str = table.concat(args, " ")
+    vim.notify("Git " .. cmd_str .. "...", vim.log.levels.INFO)
+    vim.fn.jobstart(vim.list_extend({ "git" }, args), {
+        on_exit = function(_, exit_code)
+            vim.schedule(function()
+                if exit_code == 0 then
+                    if success_msg then
+                        vim.notify(success_msg, vim.log.levels.INFO)
+                    else
+                        vim.notify("Git " .. cmd_str .. " completed", vim.log.levels.INFO)
+                    end
+                else
+                    vim.notify("Git " .. cmd_str .. " failed", vim.log.levels.WARN)
+                end
+            end)
+        end,
+    })
+end
+
 function M.setup()
     -- Insert abbreviations
     vim.cmd([[
@@ -17,22 +37,31 @@ function M.setup()
         iabbrev xtes 🧪 test():[#]<Left><Left><Left><Left>
     ]])
 
-    -- Command abbreviations
+    -- Async git command
+    vim.api.nvim_create_user_command("RkGitAsync", function(opts)
+        if #opts.fargs == 0 then
+            vim.notify("Usage: RkGitAsync <git command>", vim.log.levels.ERROR)
+            return
+        end
+        async_git(opts.fargs)
+    end, { nargs = "*", complete = "shellcmd" })
+
+    -- Command abbreviations (async)
     vim.cmd([[
-        cabbrev Gc silent G checkout <C-r><C-w>
-        cabbrev Gcherry G cherry-pick <C-r><C-w>
-        cabbrev Gclr G clean -d -f
-        cabbrev Gdell silent G branch -d
-        cabbrev Gdelr G push origin --delete
-        cabbrev Gf G fetch
-        cabbrev Gm silent G merge --ff <C-r><C-w>
-        cabbrev Gnew silent G checkout -b
-        cabbrev Gpl G pull
-        cabbrev Gps G push
-        cabbrev Gr G rebase <C-r><C-w>
-        cabbrev Gstashpo silent G stash pop
-        cabbrev Gstashpu silent G stash push --include-untracked
-        cabbrev Gtag silent G tag
+        cabbrev Gc RkGitAsync checkout <C-r><C-w>
+        cabbrev Gcherry RkGitAsync cherry-pick <C-r><C-w>
+        cabbrev Gclr RkGitAsync clean -d -f
+        cabbrev Gdell RkGitAsync branch -d
+        cabbrev Gdelr RkGitAsync push origin --delete
+        cabbrev Gf RkGitAsync fetch
+        cabbrev Gm RkGitAsync merge --ff <C-r><C-w>
+        cabbrev Gnew RkGitAsync checkout -b
+        cabbrev Gpl RkGitAsync pull
+        cabbrev Gps RkGitAsync push
+        cabbrev Gr RkGitAsync rebase <C-r><C-w>
+        cabbrev Gstashpo RkGitAsync stash pop
+        cabbrev Gstashpu RkGitAsync stash push --include-untracked
+        cabbrev Gtag RkGitAsync tag
     ]])
 end
 
